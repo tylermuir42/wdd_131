59bbd6ac01c9759ea4390879de7c0026cf528561
tylermuir42 <tylermuir42@gmail.com>
2025-09-18 10:37:38 -0600

    Completed mission statement assignment

--- mission_statement/mission.js
+++ mission_statement/mission.js
@@ -1,4 +1,25 @@
-const apples = 10;
-const oranges = 5;
+const themeSelector = document.querySelector('select');
+const logo = document.getElementById('logo');
 
-console.log(apples + oranges);
+function changeTheme() {
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+if (themeSelector.value === "dark"){
+    document.body.classList.add("dark");
+    logo.src = "byui-logo_white.png"
+} else{
+    document.body.classList.remove("dark");
+    logo.src = "byui-logo_blue.webp";
+}
+
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo img to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
---
+++ sandbox/dom_basics.js
@@ -0,0 +1,12 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("alt", "Random Image");
+newImage.src = "https://picsum.photos/200";
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
